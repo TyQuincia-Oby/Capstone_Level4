3808eeca9a78598024b4e7a32608f3bfea9b937f
TyQuincia Oby <tyquinciaoby@gmail.com>
2026-01-05 02:36:11 -0600

    changed home route json and console.log to differentiate from backend

--- microservice/index.js
+++ microservice/index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-const PORT = 3001 //run on different port from REST API/Core Server
+const PORT = 3001; //run on different port from REST API/Core Server
 
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello from microservice home route'
     })
-    console.log('Home route active')
+    console.log('Microservice home route active')
 });
 
 app.listen(PORT, () => {
